76c09ef2f0ad521bd458ce24a539033ba1578fdb
kimkwatson <kimwatson731@gmail.com>
2026-03-10 00:36:03 -0700

    changed views paths in server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,12 +8,12 @@ dotenv.config();
 
 const app = express();
 
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../src/views"));
 app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use("/projects", projectRoutes);
 
 app.get("/", (req, res) => {
