2a9f3d96152bfe31fcad61a3e79bfa52057dd09b
kimkwatson <kimwatson731@gmail.com>
2026-03-10 14:57:54 -0700

    updated file path

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../src/public")));
 app.use("/projects", projectRoutes);
 
 app.get("/", (req, res) => {
